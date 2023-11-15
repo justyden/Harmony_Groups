145a116a4baaf915a4bf82acbbbb9faa09fefb15
Tyler Thompson <tylert123@yahoo.com>
2023-11-15 13:01:01 -0500

    Added files

---
+++ src/HarmonyGroup.py
@@ -0,0 +1,5 @@
+''' An application that has common activites that make people happy. This activites are held in groups within the application
+and depending on the user that has signed in, they are allowed into the activites they belong to. These activites function as 
+group chat for people that have similiar interest with these activites. The application uses a hash table to admit users into the 
+specific table my verifying they belong to that activity.'''
+
