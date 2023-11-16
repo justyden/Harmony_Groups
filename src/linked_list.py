28b8c1c07b27db9b22bd0113ae2aa24463ecc9b1
Ryan <ry4n7501@gmail.com>
2023-11-16 13:47:34 -0500

    Created a Node, LinkedList class to deal with hash collisions

---
+++ src/linked_list.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # For inserting a new user into the LinkedList
+    def insert(self, val):
+        pass
+
+    # For removing a user from the LinkedList
+    def remove(self,val):
+        pass
