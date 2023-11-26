d9f082c13aec5281d83b3eb078dd592173e4de09
Ryan <ry4n7501@gmail.com>
2023-11-26 12:04:24 -0500

    Added documentation

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,79 +1,89 @@
+"""
+Node class that represents individual user in the
+linked list.
+"""
 class Node:
+    """
+    Node class meant to store users in the linked list.
+
+    Attributes:
+        val: str
+        next: Node
+        prev: Node
+    """
     def __init__(self, val):
+        """
+        Node class constructor
+
+        Args:
+            val: A user ID
+        """
         self.val = val
-        self.next = None
-        self.prev = None
+        self.next = None  # The node following the current node
+        self.prev = None  # The node behind the current node
 
+
+"""
+LinkedList class that builds a linked list out of
+Node objects.
+"""
 class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.last = None
+    """
+    LinkedList class designed to construct a linked list
 
-    # For inserting a new user into the LinkedList
+    Attributes:
+        head: Node
+        last: Node
+    """
+    def __init__(self):
+        """
+        LinkedList class constructor
+        """
+        self.head = None  # Head is the first Node in the list
+        self.last = None  # Last is the last Node in the list
+
     def insert(self, val):
-        # current is a variable used to traverse the linked list
+        """
+        Inserts a new user Node at the end of the LinkedList. Similar to an 'append()' function.
+
+        Args:
+            val (Node): A Node object that describes a user
+        """
         if self.head == None:
             self.head = Node(val)
-            self.last = self.head
+            self.last = self.head  # When only one node is in the LinkedList, it's both the head and last node
         else:
-            temp = self.last
+            temp = self.last  # Temporarily stores self.last's value to set self.last.prev's value
             self.last.next = Node(val)
             self.last = self.last.next
             self.last.prev = temp
 
     def display(self):
-        current = self.head
+        """
+        Prints all Nodes in the LinkedList
+        """
+        current = self.head  # current is a variable used to iterate through the LinkedList
         while current is not None:
             print(current.val, end=" ")
-            current = current.next
+            current = current.next  # Changes current's value to the next Node in the list
 
-    def display_last(self):
-        print(self.last.val)
-
-
-
-    # For removing a user from the LinkedList
     def remove(self, user):
+        """
+        Removes the specified user Node from the LinkedList
+
+        Args:
+            user (str): A string that represents the userID to be removed from the LinkedList
+        """
         current = self.head
-        # if the node to delete is the head node
-        if self.head.val == user:
-            # move self.head up to the next node
-            self.head = self.head.next
-            # set the new self.head's prev position to None
-            self.head.prev = None
-        # if node to delete is the last node
-        elif self.last.val == user:
-            # move self.last back to the prev node
-            self.last = self.last.prev
-            # set the new self.last's next to None
-            self.last.next = None
+        if self.head.val == user:  # If the Node to delete is the head Node
+            self.head = self.head.next  # Move self.head up to the next Node
+            self.head.prev = None  # Set the new self.head's prev position to None
+        elif self.last.val == user:  # If Node to delete is the last Node
+            self.last = self.last.prev  # Move self.last back to the prev Node
+            self.last.next = None  # Set the new self.last's next to None
         else:
-            # iterate over nodes to find the node to remove from list
-            while current.next:
-                # checks if the current node matches the node to be removed
-                if current.val == user:
-                    # for the node behind the current node, set its next value to current.next
-                    current.prev.next = current.next
-                    # for the node ahead of current, set its prev value to current.prev
-                    current.next.prev = current.prev
+            while current.next:  # Iterate over LinkedList to find the Node to remove
+                if current.val == user:  # Checks if the current Node matches the Node to be removed
+                    current.prev.next = current.next  # For the Node behind current, set its next value to current.next
+                    current.next.prev = current.prev  # For the Node ahead of current, set its prev value to current.prev
                 current = current.next
-
-
-user1 = "a"
-user2 = "b"
-user3 = "c"
-user4 = "d"
-LL1 = LinkedList()
-LL1.insert(user1)
-LL1.insert(user2)
-LL1.insert(user3)
-LL1.insert(user4)
-LL1.display()
-print()
-
-
-LL1.remove("b")
-print()
-LL1.display()
-# print()
-# LL1.display_last()
